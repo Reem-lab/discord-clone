4a41f87b01b27d3fa979d4fa805217a979603947
reem <janinareem@gmail.com>
2023-01-12 21:48:18 +0200

    Add sideBar channelsr component file

---
+++ src/SidebarChannel.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { setChannelInfo } from './features/appSlice';
+import './SidebarChannel.css';
+
+const SidebarChannel = ({ id, channelName}) => {
+  const dispatch = useDispatch();
+  return (
+    <div className='sidebar__channel' onClick={() => {
+      dispatch(
+        setChannelInfo({
+          channelId: id,
+          channelName: channelName,
+        })
+      )
+    }}>
+      <h4>
+        <span className='sidebarChannel__hash'>#</span> {channelName}
+      </h4>
+    </div>
+  )
+}
+
+export default SidebarChannel
